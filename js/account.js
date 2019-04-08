a5b3ba84ecc30ed1d5eef389d9a6e31fe956d438
Johanna Rousi <johanna.rousi@gmail.com>
2019-04-08 13:31:30 +0300

    corrections to personAccount-object: incomes and expenses

--- js/account.js
+++ js/account.js
@@ -1,20 +1,74 @@
+const displayDateTime = () => {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let date = now.getDate();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  let dateMonthYear = date + "." + month + "." + year;
+  let time = hours + ":" + minutes;
+  let fullTime = dateMonthYear + " " + time;
+  return fullTime;
+};
+
 const personAccount = {
   firstName: "Johanna",
   lastName: "Rousi",
-  incomes: { mainJob: 5000, sideJob: 600, passiveIncome: 800 },
-  expenses: { rent: 500, food: 400, transport: 150, hobbies: 100 },
+  incomes: [
+    {
+      id: "income",
+      description: "salary",
+      amount: 3500,
+      time: displayDateTime()
+    },
+    {
+      id: "income",
+      description: "side-job",
+      amount: 600,
+      time: displayDateTime()
+    },
+    {
+      id: "income",
+      description: "passiveIncome",
+      amount: 800,
+      time: displayDateTime()
+    }
+  ],
+  expenses: [
+    {
+      id: "expense",
+      description: "rent",
+      amount: 500,
+      time: displayDateTime()
+    },
+    {
+      id: "expense",
+      description: "transport",
+      amount: 150,
+      time: displayDateTime()
+    },
+    {
+      id: "expense",
+      description: "hobbies",
+      amount: 100,
+      time: displayDateTime()
+    }
+  ],
   totalIncomes: function() {
-    return (
-      this.incomes.mainJob + this.incomes.sideJob + this.incomes.passiveIncome
-    );
+    let sum = 0;
+    this.incomes.forEach(income => (sum += income.amount));
+    return sum;
   },
   totalExpenses: function() {
-    return (
-      this.expenses.rent +
-      this.expenses.food +
-      this.expenses.transport +
-      this.expenses.hobbies
-    );
+    let sum = 0;
+    this.expenses.forEach(expense => (sum += expense.amount));
+    return sum;
   },
   accountInfo: function() {
     return `Total of incomes: ${this.totalIncomes()}\nTotal of expenses: ${this.totalExpenses()}`;
@@ -31,3 +85,4 @@ const personAccount = {
 };
 // console.log(personAccount);
 // console.log(personAccount.accountBalance());
+console.log(personAccount.totalExpenses());
--- js/main.js
+++ js/main.js
@@ -9,25 +9,6 @@ const resultText = document.querySelector(".result-text");
 const incomeUl = document.querySelector(".income-ul");
 const expenseUl = document.querySelector(".expense-ul");
 
-const displayDateTime = () => {
-  let now = new Date();
-  let year = now.getFullYear();
-  let month = now.getMonth() + 1;
-  let date = now.getDate();
-  let hours = now.getHours();
-  let minutes = now.getMinutes();
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  let dateMonthYear = date + "." + month + "." + year;
-  let time = hours + ":" + minutes;
-  let fullTime = dateMonthYear + " " + time;
-  return fullTime;
-};
-
 // save inputs
 const saveInputs = e => {
   e.preventDefault();
